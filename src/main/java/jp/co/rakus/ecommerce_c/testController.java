21a0b1d824460fb6842b4b3f541d947a03ef729e
atsukoR <atsuko.yoshino@rakus.co.jp>
2017-08-24 18:10:37 +0900

    orderList.jspの動作確認しました。testControllerは使っても消しても構いません！

--- src/main/java/jp/co/rakus/ecommerce_c/testController.java
+++ src/main/java/jp/co/rakus/ecommerce_c/testController.java
@@ -1,13 +1,15 @@
 package jp.co.rakus.ecommerce_c;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("test")
+@Controller
+@RequestMapping("/test")
 public class testController {
 	@RequestMapping("/")
 	public String test(){
 		
-		return "order_list";
+		return "orderList";
 	}
 	
 
