434a852778ba8c470ef051970a9b6266974640c6
shun.nakano <shun.nakano@rakus.co.jp>
2017-08-25 15:55:00 +0900

    ユーザ登録処理を追加しました。登録画面表示はまだ

--- src/main/java/jp/co/rakus/ecommerce_c/controller/RegisterUserController.java
+++ src/main/java/jp/co/rakus/ecommerce_c/controller/RegisterUserController.java
@@ -1,8 +1,70 @@
 package jp.co.rakus.ecommerce_c.controller;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import jp.co.rakus.ecommerce_c.domain.User;
+import jp.co.rakus.ecommerce_c.form.UserForm;
+import jp.co.rakus.ecommerce_c.service.RegisterUserService;
+
+/**
+ * ユーザ登録処理のコントローラ.
+ * 
+ * @author shun.nakano
+ *
+ */
 @Controller
+@RequestMapping("/registerUser")
 public class RegisterUserController {
+	@Autowired
+	private RegisterUserService regiseterUserService;
+	
+	@ModelAttribute
+	public UserForm setUserForm(){
+		return new UserForm();
+	}
+	
+	/**
+	 * ユーザ登録画面を表示する.
+	 * 
+	 * @param model リクエストパラメータ
+	 * @return ユーザ登録画面
+	 */
+	@RequestMapping
+	public String registerUser(Model model){
+		return "registerUser";
+	}
+	
+	/**
+	 * 渡された情報でユーザ登録処理を行う.
+	 * 
+	 * @param form
+	 * @param result
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping("/submit")
+	private String submit(@Validated UserForm form, BindingResult result, Model model){
+		// 入力値チェック
+		if(regiseterUserService.findByEmail(form.getEmail()) != null){
+			result.rejectValue("email", null, "そのメールアドレスはすでに使われています");
+		}
+		if(!form.getPassword().equals(form.getReInputPassword())){
+			result.rejectValue("password", null, "確認欄と異なるパスワードが入力されました");
+		}
+		if(result.hasErrors()){			
+			return registerUser(model);
+		}
+		
+		User user = new User();
+		BeanUtils.copyProperties(form, user);
+		return "redirect:/login";
+	}
 
 }
