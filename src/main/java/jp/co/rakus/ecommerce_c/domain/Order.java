3ea7db4608b36322c713f5cab9da2153552b5faf
fort19 <kingstar.yokohama@gmail.com>
2017-08-28 08:57:15 +0900

    orderクラス編集しました。

--- src/main/java/jp/co/rakus/ecommerce_c/domain/Order.java
+++ src/main/java/jp/co/rakus/ecommerce_c/domain/Order.java
@@ -6,135 +6,161 @@ import java.util.List;
 
 /**
  * 注文データ
- * @author atsuko.yoshino 
+ * 
+ * @author atsuko.yoshino
  *
  */
 public class Order {
- 
- /**id*/
- private long id;
- /**ユーザーID*/
- private long userId;
- /**状態*/
- private Integer status;
- /**合計金額*/
- private Integer totalPrice;
- /**注文日*/
- private Date orderDate;
- /**宛名*/
- private String destinationName;
- /**宛先Eメール*/
- private String destinationEmail;
- /**宛先郵便番号*/
- private String destinationZipcode;
- /**宛先住所*/
- private String destinationAddress;
- /**宛先TEL*/
- private String destinationTel;
- /**配達時間*/
- private Timestamp deliveryTime;
- /**支払方法*/
- private Integer paymentMethod;
- /**ユーザー*/
- private User user;
- /**注文内容*/
- private List<OrderItem> orderItemList;
- 
- 
- public long getId() {
-  return id;
- }
- public void setId(long id) {
-  this.id = id;
- }
- public long getUserId() {
-  return userId;
- }
- public void setUserId(long userId) {
-  this.userId = userId;
- }
- public Integer getStatus() {
-  return status;
- }
- public void setStatus(Integer status) {
-  this.status = status;
- }
- public Integer getTotalPrice() {
-  return totalPrice;
- }
- public void setTotalPrice(Integer totalPrice) {
-  this.totalPrice = totalPrice;
- }
- public Date getOrderDate() {
-  return orderDate;
- }
- public void setOrderDate(Date orderDate) {
-  this.orderDate = orderDate;
- }
- public String getDestinationName() {
-  return destinationName;
- }
- public void setDestinationName(String destinationName) {
-  this.destinationName = destinationName;
- }
- public String getDestinationEmail() {
-  return destinationEmail;
- }
- public void setDestinationEmail(String destinationEmail) {
-  this.destinationEmail = destinationEmail;
- }
- public String getDestinationZipcode() {
-  return destinationZipcode;
- }
- public void setDestinationZipcode(String destinationZipcode) {
-  this.destinationZipcode = destinationZipcode;
- }
- public String getDestinationAddress() {
-  return destinationAddress;
- }
- public void setDestinationAddress(String destinationAddress) {
-  this.destinationAddress = destinationAddress;
- }
- public String getDestinationTel() {
-  return destinationTel;
- }
- public void setDestinationTel(String destinationTel) {
-  this.destinationTel = destinationTel;
- }
- public Timestamp getDeliveryTime() {
-  return deliveryTime;
- }
- public void setDeliveryTime(Timestamp deliveryTime) {
-  this.deliveryTime = deliveryTime;
- }
- public Integer getPaymentMethod() {
-  return paymentMethod;
- }
- public void setPaymentMethod(Integer paymentMethod) {
-  this.paymentMethod = paymentMethod;
- }
- public User getUser() {
-  return user;
- }
- public void setUser(User user) {
-  this.user = user;
- }
- public List<OrderItem> getOrderItemList() {
-  return orderItemList;
- }
- public void setOrderItemList(List<OrderItem> orderItemList) {
-  this.orderItemList = orderItemList;
- }
- 
- //未実装（/**支払税額*/)
- public int getTax(){
-  return (int)(this.totalPrice * 0.08);
- }
- 
- //未実装(/**支払額合計*/)
- public int getCalcTotalPrice(){
-  return this.totalPrice + getTax();
- }
- 
+
+	/** id */
+	private long id;
+	/** ユーザーID */
+	private long userId;
+	/** 状態 */
+	private Integer status;
+	/** 合計金額 */
+	private Integer totalPrice;
+	/** 注文日 */
+	private Date orderDate;
+	/** 宛名 */
+	private String destinationName;
+	/** 宛先Eメール */
+	private String destinationEmail;
+	/** 宛先郵便番号 */
+	private String destinationZipcode;
+	/** 宛先住所 */
+	private String destinationAddress;
+	/** 宛先TEL */
+	private String destinationTel;
+	/** 配達時間 */
+	private Timestamp deliveryTime;
+	/** 支払方法 */
+	private Integer paymentMethod;
+	/** ユーザー */
+	private User user;
+	/** 注文内容 */
+	private List<OrderItem> orderItemList;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(long userId) {
+		this.userId = userId;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public Integer getTotalPrice() {
+		return totalPrice;
+	}
+
+	public void setTotalPrice(Integer totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+
+	public Date getOrderDate() {
+		return orderDate;
+	}
+
+	public void setOrderDate(Date orderDate) {
+		this.orderDate = orderDate;
+	}
+
+	public String getDestinationName() {
+		return destinationName;
+	}
+
+	public void setDestinationName(String destinationName) {
+		this.destinationName = destinationName;
+	}
+
+	public String getDestinationEmail() {
+		return destinationEmail;
+	}
+
+	public void setDestinationEmail(String destinationEmail) {
+		this.destinationEmail = destinationEmail;
+	}
+
+	public String getDestinationZipcode() {
+		return destinationZipcode;
+	}
+
+	public void setDestinationZipcode(String destinationZipcode) {
+		this.destinationZipcode = destinationZipcode;
+	}
+
+	public String getDestinationAddress() {
+		return destinationAddress;
+	}
+
+	public void setDestinationAddress(String destinationAddress) {
+		this.destinationAddress = destinationAddress;
+	}
+
+	public String getDestinationTel() {
+		return destinationTel;
+	}
+
+	public void setDestinationTel(String destinationTel) {
+		this.destinationTel = destinationTel;
+	}
+
+	public Timestamp getDeliveryTime() {
+		return deliveryTime;
+	}
+
+	public void setDeliveryTime(Timestamp deliveryTime) {
+		this.deliveryTime = deliveryTime;
+	}
+
+	public Integer getPaymentMethod() {
+		return paymentMethod;
+	}
+
+	public void setPaymentMethod(Integer paymentMethod) {
+		this.paymentMethod = paymentMethod;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public List<OrderItem> getOrderItemList() {
+		return orderItemList;
+	}
+
+	public void setOrderItemList(List<OrderItem> orderItemList) {
+		this.orderItemList = orderItemList;
+	}
+
+	// 未実装（/**支払税額*/)
+	public int getTax() {
+		return (int) (this.totalPrice * 0.08);
+	}
+
+	// 未実装(/**支払額合計*/)
+	public int getCalcTotalPrice() {
+		return this.totalPrice + getTax();
+	}
 
 }
