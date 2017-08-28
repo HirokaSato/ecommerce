4939ca283efedf905f6312dcfd99d573ea85e59f
shun.nakano <shun.nakano@rakus.co.jp>
2017-08-28 21:10:59 +0900

    共通しているJSPヘッドナビ部分をincludeで記述できるようにした

--- src/main/java/jp/co/rakus/ecommerce_c/controller/OrderHistoryController.java
+++ src/main/java/jp/co/rakus/ecommerce_c/controller/OrderHistoryController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping
 public class OrderHistoryController {
 	
-	@RequestMapping("/orderhistory")
+	@RequestMapping("/orderHistory")
 	public String orderHistory(){
 		
 		return "orderHistory" ;
